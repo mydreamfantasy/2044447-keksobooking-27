cb56af7945bc2a1025843c17482b2cbd15e9cd70
Anastasiia <nastyara13@yandex.ru>
2022-09-30 18:05:13 +0300

    Обновлены функции

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-//Функция возврата целого числа, взято из MDN
+// функция возврата целого числа, взято из MDN
 
-function getWholeNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-getWholeNumber(78, 99);
-
-// функция возврата положительного числа с плаваующей точкой
-let fractionalNumber;
-
-function getNumber(min, max, round) {
-  if(min >= 0 && max >= 0) {
-    if(min > max) {
-      fractionalNumber = ((Math.random() * (min - max + 1)) + max).toFixed(round);
-      return Number(fractionalNumber);
-    }
-    fractionalNumber = ((Math.random() * (max - min + 1)) + min).toFixed(round);
-    return Number(fractionalNumber);
+const getRandomInteger = (min, max) => {
+  if(min >= 0 && max >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return NaN;
-}
 
-(getNumber(5555, 669.8, 6));
+  return NaN;
+};
+
+getRandomInteger(33, 45);
+
+// функция возврата положительного числа с плавающей точкой
+const getRandomFloat = (min, max, round) => {
+  if(min < 0 || max < 0 || max < min) {
+
+    return NaN;
+  }
+  const floatNumber = ((Math.random() * (max - min + 1)) + min).toFixed(round);
+
+  return Number(floatNumber);
+};
+
+getRandomFloat(555, 669.8, 2);
