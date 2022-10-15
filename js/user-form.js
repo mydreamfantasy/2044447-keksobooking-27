b953ba7b65ba4e91a4053ba46cf9518dccbf24b7
Anastasiia <nastyara13@yandex.ru>
2022-10-15 22:13:58 +0300

    Исправление замечаний в активации страницы

--- js/user-form.js
+++ js/user-form.js
@@ -1,46 +1,38 @@
 
-const addAttr = (formField) => {
-  formField.forEach((field) => {
-    field.setAttribute('disabled', 'disabled');
-  });
-};
+const form = document.querySelector('.ad-form');
+const formFieldsets = form.querySelectorAll('fieldset');
+const mapForm = document.querySelector('.map__filters');
+const mapFieldsets = mapForm.querySelectorAll('fieldset, select');
 
-const removeAttr = (formField) => {
-  formField.forEach((field) => {
-    field.removeAttribute('disabled');
+
+const toggleAttr = (formFields) => {
+  formFields.forEach((field) => {
+
+    if(field.disabled) {
+      field.disabled = true;
+    } else {
+      field.disabled = false;
+    }
   });
 };
 
 const activatePage = (active) => {
-  const form = document.querySelector('.ad-form');
-  const formHeaders = form.querySelectorAll('.ad-form-header');
-  const formFieldsets = form.querySelectorAll('.ad-form__element');
+  toggleAttr(formFieldsets);
 
   if (active) {
-    removeAttr(formFieldsets);
-    removeAttr(formHeaders);
     form.classList.remove('ad-form--disabled');
-
   } else {
-    addAttr(formFieldsets);
-    addAttr(formHeaders);
     form.classList.add('ad-form--disabled');
   }
 };
 
 const activateFilter = (mapLoad) => {
-  const mapForm = document.querySelector('.map__filters');
-  const mapFormSelects = mapForm.querySelectorAll('.map__filter');
-  const mapFieldsets = mapForm.querySelectorAll('.map__features');
+  toggleAttr(mapFieldsets);
 
   if (mapLoad) {
-    removeAttr(mapFormSelects);
-    removeAttr(mapFieldsets);
     mapForm.classList.remove('map__filters--disabled');
   } else {
     mapForm.classList.add('map__filters--disabled');
-    addAttr(mapFormSelects);
-    addAttr(mapFieldsets);
   }
 };
 
