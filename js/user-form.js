f3d449bee7aa007cbe5389a628cbef205b65a317
Anastasiia <nastyara13@yandex.ru>
2022-10-18 23:17:34 +0300

    Валидация. Рефакторинг

--- js/user-form.js
+++ js/user-form.js
@@ -3,94 +3,14 @@ const form = document.querySelector('.ad-form');
 const formFieldsets = form.querySelectorAll('fieldset');
 const mapForm = document.querySelector('.map__filters');
 const mapFieldsets = mapForm.querySelectorAll('fieldset, select');
-const roomField = form.querySelector('[name="rooms"]');
-const guestField = form.querySelector('[name="capacity"]');
-const priceField = form.querySelector('#price');
 
-const pristine = new Pristine(form, {
-  classTo: 'ad-form__element',
-  errorClass: 'ad-form__element--invalid',
-  successClass: 'ad-form__element--valid',
-  errorTextParent: 'ad-form__element',
-  errorTextTag: 'span',
-  errorTextClass: 'text-help'
-}, false);
-
-const validateTitle = (value) => value.length >= 30 && value.length <= 100;
-
-pristine.addValidator(
-  form.querySelector('#title'),
-  validateTitle,
-  'От 30 до 100 символов'
-);
-
-const minPrice = {
-  'bungalow': '0',
-  'flat': '1000',
-  'hotel': '3000',
-  'house': '5000',
-  'palace': '10000'
-};
-
-const validatePrice = (value) => {
-  const unit = form.querySelector('[name="type"]');
-  return value.length && parseInt(value, 10) >= minPrice[unit.value];
-};
-
-const getPriceErrorMessage = () => {
-  const unit = form.querySelector('[name="type"]');
-  return (`Не меньше ${minPrice[unit.value]} рублей`);
-};
-
-pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
-
-const onUnitChange = () => {
-  priceField.placeholder = minPrice[this.value];
-  pristine.validate(priceField);
-};
-
-form.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onUnitChange));
-
-const roomOption = {
-  '1': ['1'],
-  '2': ['2', '1'],
-  '3': ['3', '2', '1'],
-  '100': ['0'],
-};
-
-const validateRooms = () => roomOption[roomField.value].includes(guestField.value);
-
-const getRoomErrorMessage = () => `${roomField.value === '100' ? 'Выберете "100 комнат"' : 'Выберете другое количество комнат'}`;
-
-const getGuestErrorMessage = () => `${roomField.value === '100' ? 'Выберете "не для гостей"' : 'Выберете другое количество гостей'}`;
-
-pristine.addValidator(roomField, validateRooms, getGuestErrorMessage);
-pristine.addValidator(guestField, validateRooms, getRoomErrorMessage);
-
-const onCheckChange = (e) => {
-  form.timein.value = e.target.value;
-  form.timeout.value = e.target.value;
-};
-
-pristine.addValidator(form.addEventListener('change', onCheckChange));
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
-
-const toggleAttr = (formFields) => {
+const toggleAttr = (formFields, state) => {
   formFields.forEach((field) => {
-
-    if(field.disabled) {
-      field.disabled = true;
-    } else {
-      field.disabled = false;
-    }
+    field.disabled = state;
   });
 };
 
-const activatePage = (active) => {
+const changePageState = (active) => {
   toggleAttr(formFieldsets);
 
   if (active) {
@@ -100,7 +20,7 @@ const activatePage = (active) => {
   }
 };
 
-const activateFilter = (mapLoad) => {
+const changeFilterState = (mapLoad) => {
   toggleAttr(mapFieldsets);
 
   if (mapLoad) {
@@ -110,4 +30,4 @@ const activateFilter = (mapLoad) => {
   }
 };
 
-export { activatePage, activateFilter };
+export { changePageState, changeFilterState };
